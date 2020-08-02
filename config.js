30011e7706e73396dae5a4e1fc69b257cf7590c2
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2020-08-02 14:22:28 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusionServer'
+    'mongoUrl': 'mongodb://localhost:27017/conFusionServer',
+    'facebook' : {
+        clientId: '916640618836675',
+        clientSecret:'84602d0c77e4dab14b38f7ce08877655'
+
+    }
 }
 
 
