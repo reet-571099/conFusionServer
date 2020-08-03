c211ea004277c72426049268df3ff8a271e10594
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2020-08-04 01:28:45 +0530

    Assignment-4

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,17 @@
 var express = require('express');
+var router = express.Router();
+
 const bodyParser = require('body-parser');
 var User = require('../models/user');
+
 var passport = require('passport');
 var authenticate = require('../authenticate');
 const cors = require('./cors');
-var router = express.Router();
+
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', cors.corsWithOptions,authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
   User.find({})
   .then((users) => {
       res.statusCode = 200;
@@ -18,8 +21,7 @@ router.get('/', cors.corsWithOptions,authenticate.verifyUser, authenticate.verif
   .catch((err) => next(err));
 });
 
-
-router.post('/signup',cors.corsWithOptions, (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
     if(err) {
@@ -29,11 +31,11 @@ router.post('/signup',cors.corsWithOptions, (req, res, next) => {
     }
     else {
       if (req.body.firstname)
-         user.firstname = req.body.firstname;
+        user.firstname = req.body.firstname;
       if (req.body.lastname)
-         user.lastname = req.body.lastname;
-      user.save((err,user) =>{
-        if(err) {
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if (err) {
           res.statusCode = 500;
           res.setHeader('Content-Type', 'application/json');
           res.json({err: err});
@@ -49,16 +51,14 @@ router.post('/signup',cors.corsWithOptions, (req, res, next) => {
   });
 });
 
-
-router.post('/login',cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
-
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {  
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', cors.corsWithOptions,(req, res) => {
+router.get('/logout', cors.corsWithOptions, (req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
@@ -81,6 +81,3 @@ router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res
 });
 
 module.exports = router;
-
-
-
