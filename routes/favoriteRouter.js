dea0eb92091109f9f46366c504d7ae71752f2deb
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2020-08-08 14:11:49 +0530

    Integrating Client and Server.

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -69,8 +69,28 @@ favoriteRouter.route('/')
 favoriteRouter.route('/:dishId')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
-    res.statusCode = 403;
-    res.end('GET operation not supported on /favorites/'+ req.params.dishId);
+    Favourites.findOne({ user:req.user._id})
+    .then((favorites) => {
+        if(!favorites) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type' ,'application/json');
+            return res.json({"exists": false,"favorites": favorites});
+        }
+        else {
+            if(favorites.dishes.indexOf(req.param.dishId) < 0) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type' ,'application/json');
+                return res.json({"exists": false,"favorites": favorites});    
+            }
+            else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type' ,'application/json');
+                return res.json({"exists": true,"favorites": favorites});
+    
+            }
+        }
+    },(err) => next(err))
+    .catch((err) =>next(err))
 })
 .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     Favorites.findOne({user: req.user._id})
@@ -308,4 +328,4 @@ module.exports = favoriteRouter;
 //             .catch((err) => next(err));
 //     });
 
-// module.exports = favoriteRouter;
+//module.exports = favoriteRouter;
